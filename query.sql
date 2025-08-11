0ddb56637bba0d219617707f8ba943bced03a924
rpsnrpsn <rpsnrpsn@naver.com>
2025-08-11 11:51:40 +0900

    20250811 3교시

---
+++ query.sql
@@ -0,0 +1,10 @@
+# 1. 스키마 생성
+DROP DATABASE IF EXISTS k5_iot_springboot;
+
+# 2. 스키마 생성 + 문자셋/정렬 설정
+CREATE DATABASE IF NOT EXISTS k5_iot_springboot
+	CHARACTER SET utf8mb4
+    COLLATE utf8mb4_general_ci;
+    
+# 3. 스키마 선택
+USE k5_iot_springboot;
