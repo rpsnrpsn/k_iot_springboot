99278362718143fad10ed23fc5a0e1458aef3c9f
rpsnrpsn <rpsnrpsn@naver.com>
2025-08-11 15:49:20 +0900

    20250811 6교시

--- src/main/java/com/example/k5_iot_springboot/이론/E_IoC_DI.java
+++ src/main/java/com/example/k5_iot_springboot/이론/E_IoC_DI.java
@@ -1,12 +1,12 @@
 package com.example.k5_iot_springboot.이론;
 
 /*
-    == 제어의 역전(IoC) VS 의존성 주입(DI) ==
+    === 제어의 역전(IoC) VS 의존성 주입(DI) ===
 
     1. 제어의 역전 (Inversion of Control)
     : 프로그램의 제어 흐름을 개발자가 직접 통제하지 않고
         , 외부 컨테이너(스프링 컨테이너)에 위임하는 방식
-    - 제어의 권한이 컨테이너에게 있어 객체의 생명 주기를 컨테이너가 분리
+    - 제어의 권한이 컨테이너에게 있어 객체의 생명 주기를 컨테이너가 관리
     - IoC 자체는 개념(이론), 이를 구현하는 대표적인 방법 DI(의존성 주입)
 
     2. 의존성 주입 (Dependency Injection)
@@ -58,8 +58,8 @@ class BookStore1 {
 class Book2 {
     private String title;
 
-    public  Book2() {
-        this.title= "스프링 기초";
+    public Book2() {
+        this.title = "스프링 기초";
     }
 
     public String getTitle() {
@@ -72,8 +72,8 @@ class BookStore2 {
     private Book2 book;
 
     // 스프링이 Book2 객체를 생성하여 자동으로 BookStore2에 넣어줌 (매개변수로 전달)
-    // - 개발자가 new 연선자 사용 없이, 스프링 컨테이너가 객체를 직접 만들어서 '주입'
-    @Autowired // 스프링 컨테이너에게 해당 타입의 빈을 찾아 주입하는 어노테이션
+    // - 개발자가 new 연산자 사용 없이, 스프링 컨테이너가 객체를 직접 만들어서 '주입'
+    @Autowired // 스프링 컨테이너에게 해당 타입의 밴을 찾아 주입하는 어노테이션
     public BookStore2(Book2 book) {
         this.book = book;
     }
@@ -84,20 +84,20 @@ class BookStore2 {
 }
 
 // 1. 스프링 컨테이너
-// : 어플리케이션 내에서 객체(빈, Bean)의 생명 주기와 설정을 관리
+// : 애플리케이션 내에서 객체(빈, Bean)의 생명 주기와 설정을 관리
 // - 제어의 역전을 실현, 의존성 주입을 지원
 
 // 2. 빈(Bean)
 // : 스프링 컨테이너가 관리하는 객체
 // - 등록 방식
-//      > 컴포넌트 스캔 기반 @Component
+//      > 컴포넌트 스캔 기반 @Component (클래스 단위 등록)
 //          + 특화 어노테이션 @Controller, @Service, @Repository 등
 //              : 내부에 @Component 포함
 //      > 자바 설정 기반 @Configuration 클래스 안에 @Bean 메서드로 수동 등록 (메서드 단위 등록)
 
 // - 네이밍 규칙
 //      > 클래스명의 첫 글자를 소문자로 바꾼 이름이 기본 빈 이름
-//      ex) Book -> book, BookStore -> bookstore 등
+//      EX) Book -> book, BookStore -> bookStore 등
 //          이름 설정 @Component("bookSpecial")
 
 @Configuration
@@ -109,7 +109,7 @@ class ExampleConfig {
 }
 
 /*
-    == 의존성 주입 방식 (생성자, 필드, 세터) ==
+    === 의존성 주입 방식 (생성자, 필드, 세터) ===
     1) 생성자 주입 방식 (권장)
         - 불변 보장 (final 가능)
         - 순환 참조 조기 감지
@@ -123,9 +123,6 @@ class ExampleConfig {
 
     3) 세터 주입 방식
         - 불변성 약화, 객체가 불완전 상태로 생성될 가능성 존재
-
-
 */
-
 public class E_IoC_DI {
 }
