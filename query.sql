d7368e0986ee7f2f036c470ad14fdbafe277fabb
rpsnrpsn <rpsnrpsn@naver.com>
2025-08-13 10:25:00 +0900

    20250813 2교시

--- query.sql
+++ query.sql
@@ -22,4 +22,22 @@ CREATE TABLE IF NOT EXISTS students (
 	name VARCHAR(100) NOT NULL,
 	email VARCHAR(100) NOT NULL UNIQUE
     );
+SELECT * FROM students;
+
+# 0813 (C_Book)
+CREATE TABLE IF NOT EXISTS books (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    writer VARCHAR(50) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    content VARCHAR(500) NOT NULL,
+    category VARCHAR(20) NOT NULL,
+	# 자바 enum 데이터 처리
+    # : DB 에서는 VARCHAR(문자열)로 관리 + CHECK 제약 조건으로 문자 제한
+    CONSTRAINT chk_book_category CHECK (category IN ('NOVEL', 'ESSAY', 'POEM', 'MAGAZINE')),
+    # 같은 저자 + 동일 제목 중복 저장 방지
+    CONSTRAINT uk_book_writer_title UNIQUE (writer, title)
+    );
+    SELECT * FROM books;
+
+    
     
