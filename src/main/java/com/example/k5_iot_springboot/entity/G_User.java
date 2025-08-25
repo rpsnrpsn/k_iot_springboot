14410d2d8e15ef03190f3d7f2cdd1b04c57847eb
rpsnrpsn <rpsnrpsn@naver.com>
2025-08-25 11:05:00 +0900

    20250825 2교시

--- src/main/java/com/example/k5_iot_springboot/entity/G_User.java
+++ src/main/java/com/example/k5_iot_springboot/entity/G_User.java
@@ -8,32 +8,56 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/*
+* User 엔티티
+* - 테이블(users)외 1:1 매핑
+* - 생성/수정 시간은 BaseTimeEntity에서 자동 세팅
+* - UserDetails 책임은 분리 (별도의 어뎁터/매퍼가 담당)
+* */
+
+
 @Entity
-@Table(name = "users")
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_users_email", columnNames = "email"),
+                @UniqueConstraint(name = "uk_users_login_id", columnNames = "login_id"),
+                @UniqueConstraint(name = "uk_users_nickname", columnNames = "nickname")
+        }
+)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+// 기본 생성자 생성 - 외부 new 방지, JPA 프록시/리플렉션용
+// cf) 프록시(proxy): 객체의 대리인 역할, 리플렉션(reflection): 객체의 정보를 동적으로 가져오고 조작하는 기술
 public class G_User extends BaseTimeEntity {
 
+    // PK: 고유번호
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false)
     private Long id;
 
-    @Column(name = "login_id", unique = true, updatable = false, nullable = false, length = 50)
+    // 로그인 아이디(유니크)
+    @Column(name = "login_id", updatable = false, nullable = false, length = 50)
     private String loginId;
 
+    // 로그인 비밀번호 (해시 저장 권장 - 암호화)
     @Column(name = "password", nullable = false, length = 255)
     private String password;
 
+    // 이메일(유니크)
     @Column(name = "email", nullable = false, length = 255)
     private String email;
 
+    // 닉네임(유니크)
     @Column(name = "nickname", nullable = false, length = 50)
     private String nickname;
 
+    // 성별 (선택, NULL 허용)
     @Enumerated(EnumType.STRING)
     @Column(name = "gender", length = 20)
     private Gender gender;
 
+    // 생성 편의 메서드
     @Builder
     private G_User(String loginId, String password, String email, String nickname, Gender gender) {
         this.loginId = loginId;
@@ -43,6 +67,7 @@ public class G_User extends BaseTimeEntity {
         this.gender = gender;
     }
 
+    // 필요 시 변경(수정) 메서드
     public void changePassword(String password) {
         this.password = password;
     }
